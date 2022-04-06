55b9d6b51c6c6742e28a0c9211ada841d5719502
jz1300 <junze1300@gmail.com>
2022-04-06 13:55:25 +0800

    commented out history in VueRouter

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),routes,
+    //history: createWebHistory(process.env.BASE_URL),
+    routes,
 })
 
 export default router
